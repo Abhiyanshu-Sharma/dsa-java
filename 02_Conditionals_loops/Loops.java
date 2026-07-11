cb61c79868e3e11359ad67f14f9b305258f4276d
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-07-11 20:59:26 +0530

    Completed Conditionals, Loops and Questions like Largest number, CaseCheck And Fibonacci

---
+++ 02_Conditionals_loops/Loops.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+         /*
+            Syntax of for loops:
+
+            for (initialisation; condition; increment/decrement) {
+                // body
+            }
+        */
+
+        // Q: print numbers from 1 to 5
+
+        // for(int n = 1; n <= 5; n++) {
+        //     System.out.println(n);
+        // }
+
+        // ###################################################################
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // for (int i = 1; i <= n; i++) {
+        //     // System.out.print(i + " ");
+        //     System.out.println("Hello World");
+        // }
+
+        // ###################################################################
+
+        // while loops
+        /*
+            Syntax:
+            while (condition) {
+                // body
+            }
+         */
+
+        // int n = 1;
+        // while (n <= 5) {
+        //     System.out.println(n);
+        //     n++;
+        // }
+
+        // ###################################################################
+
+        // do while
+        /*
+
+            do {
+                // body
+            } while (condition);
+
+         */
+
+            int n = 1;
+        do {
+            System.out.println("Hello World");
+        } while (n != 1);
+
+        // sc.close();
+        
+    }
+}
