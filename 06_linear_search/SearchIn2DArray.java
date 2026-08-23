63254e6ceddccebd098b82474d844dee91c80cd0
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-08-24 01:21:50 +0530

    Linear Search - Find number of digits Lc 1295 also completed

--- 06_linear_search/SearchIn2DArray.java
+++ 06_linear_search/SearchIn2DArray.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SearchIn2DArray {
     public static void main(String[] args) {
         int[][] arr = {
@@ -6,5 +9,37 @@ public class SearchIn2DArray {
                 {78, 99, 34, 56},
                 {18, 12}
         };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int target = sc.nextInt();
+        System.out.println();
+        int[] ans = search(arr, target);
+
+        System.out.println(Arrays.toString(ans));
+        System.out.println("Max number in the array: " + max(arr));
+        sc.close();
+    }
+
+    static int[] search(int[][] arr, int target) {
+        for(int row = 0; row < arr.length; row++) {
+            for(int col = 0; col < arr[row].length; col++) {
+                if(arr[row][col] == target) {
+                    return new int[] {row, col};
+                }
+            }
+        }
+        return new int[] {-1, -1};
+    }
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int element : ints) {
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DSA in Java
 
 Learning Data Structure and Algorithms in Java
-
+Hello chippy chippy chippy. What is the timeout time continue continuum by block private host private hosting benzene? Let it let us skip let us skip that continue your story medium size index index of CSS is available. I think we are I think we are saying the light is allowed yeah very good Yahoo regarding the next step the person has to write in all aspects as in the description what helps the right of at the same time it will be zero must change the main order detective chapter letters I think therefore I am set a combination justify additional process to justify the ability of the cost of constructing compared to natural properties that is much harder and much more interesting website measure measures required to take model rapid application development which is used when you want to develop a application very fast very fast time efficient risk factors Hello Ninety
 - Following Kunal Kushwaha's DSA playlist
 - Practicing concepts
 - Solving Problems
