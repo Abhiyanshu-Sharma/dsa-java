b87d61020e89292e03004b40aa6c923fc55dc815
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-09-02 22:27:19 +0530

    Binary Search - Ceiling, Floor, LC_034 and LC_744

---
+++ LeetCode/Binary Search/LC_034_Find_FirstLast_Element_of_Array.java
@@ -0,0 +1,38 @@
+public class LC_034_Find_FirstLast_Element_of_Array {
+    int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        
+        // Check for the first occurence
+
+        ans[0] = search(nums, target, true);
+        if(ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+
+        return ans;
+    }
+
+    int search(int[] nums, int target, boolean firstIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid]) {
+                end = mid - 1 ;
+            } else if(target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // Potential ans found
+                ans = mid;
+                if(firstIndex) {
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ LeetCode/Binary Search/LC_744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,19 @@
+// Very Similar to Ceiling.java in Binary Search 
+
+public class LC_744_Find_Smallest_Letter_Greater_Than_Target {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
